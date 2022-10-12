109407f3fba9a51b93c790fdf204ff3b562a9dae
Vitaly <woodanon123@gmail.com>
2022-10-12 14:56:00 +0500

    creata jenkinsfile add template profile

---
+++ src/main/java/ru/gb/hubr/api/security/UserDto.java
@@ -0,0 +1,39 @@
+package ru.gb.hubr.api.security;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+    @JsonIgnore
+    private Long id;
+    @NotBlank
+    @Size(min = 3, message = "username length must be greater than 2 symbols")
+    private String username;
+    @NotNull(message = "is required")
+    @Size(min = 8, message = "required 8 symbols")
+    private String password;
+    @NotNull(message = "is required")
+    @Size(min = 8, message = "required 8 symbols")
+    private String matchingPassword;
+    @NotBlank(message = "is required")
+    private String firstname;
+    @NotBlank(message = "is required")
+    private String lastname;
+    @Email
+    @NotBlank(message = "is required")
+    private String email;
+    @NotBlank(message = "is required")
+    @Size(min = 5, message = "min 5 symbols")
+    private String phone;
+}
